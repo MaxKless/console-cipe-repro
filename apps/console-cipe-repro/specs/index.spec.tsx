0654772d81dde38e3711e51a1e28aedabdaf4232
Max Kless <maxk@nrwl.io>
2024-11-28 13:30:07 +0100

    change test

--- apps/console-cipe-repro/specs/index.spec.tsx
+++ apps/console-cipe-repro/specs/index.spec.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Page from '../src/app/page';
 
 describe('Page', () => {
-  it('should render successfully', () => {
+  it('should render successfullyy', () => {
     const { baseElement } = render(<Page />);
     expect(baseElement).toBeFalsy();
   });
